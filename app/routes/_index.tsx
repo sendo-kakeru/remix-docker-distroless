095c14a2307ce3d0c2b34126237b833f71cef32d
sendo-kakeru <sendokakeru.js@gmail.com>
2024-09-02 21:39:57 +0900

    add prisma

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,10 @@
-import type { MetaFunction } from "@remix-run/node";
+import { PrismaClient } from "@prisma/client";
+import {
+  json,
+  type ActionFunctionArgs,
+  type MetaFunction,
+} from "@remix-run/node";
+import { redirect, useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -6,43 +12,37 @@ export const meta: MetaFunction = () => {
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
+const prisma = new PrismaClient();
+export async function loader() {
+  const todos = await prisma.todo.findMany();
+  return json({ todos });
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  const createdTodo = await prisma.todo.create({
+    data: { title: formData.get("title") as string },
+  });
+  console.log("createdTodo", createdTodo);
+  return redirect(".");
+}
 
 export default function Index() {
+  const loaderData = useLoaderData<typeof loader>();
   return (
     <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <form method="POST" action="/?index">
+        <input type="text" name="title" />
+        <button type="submit">submit</button>
+      </form>
+      <div>
+        <p>Todo</p>
+        <ul>
+          {loaderData.todos.map((todo) => (
+            <li key={todo.id}>{todo.title}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
